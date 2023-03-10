3354e2989e125fddf780742b121af0bcc7c6bb4c
renan8976 <126109927+renan8976@users.noreply.github.com>
2023-03-10 15:14:23 -0300

    Add files via upload

---
+++ oficina02.js
@@ -0,0 +1,16 @@
+let temDinheiro = true;
+let temSol = false;
+
+if (temDinheiro) {
+   if (temSol) {
+     console.log("Guaruja");
+   } else {
+    console.log("Restaurante");
+   }
+} else {
+  if (temsol) {
+     console.log("Ibirapuera");
+  }  else {
+     console.log("Aula");
+  }
+}
---
+++ oficina03.js
@@ -0,0 +1,8 @@
+if(true){
+    let a =10;
+    console.log(a);
+
+}
+
+//console.log(a) fora do escopo
+//diferença entre let a var(diferença de escopo
